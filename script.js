55957ff0c9f6c52954449101ad76759add9853ae
rich-taveras <rtaveras@yahoo.com>
2023-09-08 18:18:01 -0400

    create social media icons

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
       $(this).toggleClass('paused');
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        var currentScroll = window.pageYOffset;
+
+        document.querySelectorAll('.seccion').forEach(function(seccion) {
+            if (seccion.offsetTop <= currentScroll && (seccion.offsetTop + seccion.offsetHeight) > currentScroll) {
+                var seccionID = seccion.id;
+                window.location.hash = '#' + seccionID;
+            }
+        });
+    });
+});
+
   
